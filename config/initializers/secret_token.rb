3d21587083b0ea4b00a9f61df4b56d1b35ec1b30
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-10-31 21:44:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetRock::Application.config.secret_key_base = '9fc3d3a7a5518fb63cca0771031b1a0e048a6886003a28d46f94f694dea19922eb49597f07213643711b81cd38306a885b063d296159c95aff2c44be95e23427'
