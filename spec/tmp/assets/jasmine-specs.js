26df1c16c3643b2f79987d60bfcdaa7a7fd08fee
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-11-03 22:21:59 -0800

    jasmine passing

--- spec/tmp/assets/jasmine-specs.js
+++ spec/tmp/assets/jasmine-specs.js
@@ -7,6 +7,21 @@ beforeEach(function() {
     }
   })
 });
+describe('jasmine is installed', function() {
+  it('passes truth test', function() {
+    expect(1 + 1).toBe(2);
+    // phantom.exit();
+  })
+  it('has required dependencies', function() {
+    expect(Backbone).toBeDefined();
+    expect(_).toBeDefined();
+    expect(Backbone.Marionette).toBeDefined();
+  })
+})
+
+
+// RAILS_ENV=test bundle exec rake spec:javascript
+;
 describe("Player", function() {
   var player;
   var song;
